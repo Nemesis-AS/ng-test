4bc87f024eda1b817deef2663f40bcbe74d32f8a
Akshat Shukla <56386613+Nemesis-AS@users.noreply.github.com>
2025-02-18 13:17:30 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
 
+interface Task {
+  title: string;
+  completed: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'first-app';
+  currentTaskText: string = '';
+  tasks: Task[] = [];
+
+  constructor() {
+    this.loadFromLocalStorage();
+  }
+
+  addTask() {
+    this.tasks.push({
+      title: this.currentTaskText,
+      completed: false,
+    });
+
+    this.currentTaskText = '';
+    this.saveToLocalStorage();
+  }
+
+  updateCompleted(taskObj: Task) {
+    for (const task of this.tasks) {
+      if (task === taskObj) {
+        task.completed = !task.completed;
+      }
+    }
+
+    this.saveToLocalStorage();
+  }
+
+  deleteTask(taskObj: Task) {
+    this.tasks = this.tasks.filter((task) => !(task === taskObj));
+
+    this.saveToLocalStorage();
+  }
+
+  saveToLocalStorage() {
+    localStorage.setItem('ng-tasks', JSON.stringify(this.tasks));
+  }
+
+  loadFromLocalStorage() {
+    if (!localStorage.getItem('ng-tasks')) return;
+
+    this.tasks = JSON.parse(localStorage.getItem('ng-tasks') || '[]');
+  }
 }
